5a9689698ee816617a438c9d8d2cfe72c5018e51
Jerricke <eejerrickw@gmail.com>
2023-06-12 15:10:07 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+const saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`
+}
+
+// const wrapAdjective = (initializer="*") => {
+//     let adj;
+//     if (initializer === "*") {  
+//         adj = "a hard worker";
+//     }   else if (initializer === "||") {
+//         adj = "a dedicated programmer";
+//     }
+//     return `You are ${initializer}${adj}${initializer}!`
+// }
+
+const wrapAdjective = (initializer = "*") => {
+    return (function(message){
+        return `You are ${initializer}${message}${initializer}!`
+    })
+}
+
+console.log(wrapAdjective('*'));
